65c007a0db5c1128f6541f874b8a14d4d8102468
guillermoholgado <guillermo.holgado1@gmail.com>
2026-09-22 10:34:25 +0200

    Project's structure for chat app

---
+++ MyFirstWebApplication/public_html/JavaScript/chatController.js
@@ -0,0 +1,11 @@
+/* 
+ * Script for page controller
+ */
+/*
+ * 
+ */
+function actualizarMensajes(mensaje){
+    
+}
+
+
---
+++ MyFirstWebApplication/public_html/JavaScript/mensaje.js
@@ -0,0 +1,13 @@
+/* 
+ * This file contains class Mensaje for a chat app
+ * @author Guillermo Holgado
+ */
+class Mensaje{
+    /**
+     * This field is intended to store message's text
+     * @type String
+     */
+    texto="";
+}
+
+
